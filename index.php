365dbb9fd21a22a1206d0d00b5f610d900676b1b
Dayhun <lanna05@naver.com>
2023-06-06 15:25:49 -0700

    merge prepare

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require_once("inc/MainPage.class.php");
 require_once("inc/RestaurantPage.class.php");
 
 echo Page::htmlStart();
-echo MainPage::mainPageHeaderLogOut();
-echo RestaurantPage::restaurantTopSection();
-echo RestaurantPage::restaurantMenuSection();
-echo MainPage::mainPageFooter();
+// echo MainPage::mainPageHeaderLogOut();
+// echo RestaurantPage::restaurantTopSection();
+// echo RestaurantPage::restaurantMenuSection();
+// echo MainPage::mainPageFooter();
 echo Page::htmlEnd();
