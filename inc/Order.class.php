21450c42f12e4c612d536cd435e2fea33f47b7b4
Jacques Ehrardt <ehrardt38@gmail.com>
2023-06-06 08:21:58 -0700

    Add no orders constructor

--- inc/Order.class.php
+++ inc/Order.class.php
@@ -3,8 +3,8 @@
 class Order{
    public static function orderBody(){
       $orderBody = '
-         <h2>Past Order</h2>
-         <main>
+         <main class="order">
+            <h2>Past Order</h2>
             <section>
                <figure>
                   <img src="./img/logo2.png" alt="">
@@ -33,9 +33,19 @@ class Order{
                   <a href="">Reorder</a>
                   <a href="">Rate your order</a>
                </aside>
-            </section>
+            </section> 
          </main>
       ';
       return $orderBody;
+   }
+
+   public static function noOrder(){
+      $noOrder = "
+         <main class='order'>
+            <h2>You don't have any orders yet</h2>
+         </main>
+      ";
+
+      return $noOrder;
    }
 }
