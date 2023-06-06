d591126341b8a96025f6cb8ef4c79597bf731e9e
Dayhun <lanna05@naver.com>
2023-06-06 15:14:33 -0700

    merge prepare

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ require_once("inc/Page.class.php");
 require_once("inc/SignInPage.class.php");
 
 echo Page::htmlStart();
-echo SignInPage::signIn();
+// echo SignInPage::signIn();
 echo Page::htmlEnd();
