7215078b65baf030ad56fbc7e16bf7f35534e78a
Dayhun <lanna05@naver.com>
2023-06-06 15:20:47 -0700

    merge prepare

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ require_once("inc/Page.class.php");
 require_once("inc/MainPage.class.php");
 
 echo Page::htmlStart();
-echo MainPage::mainPageHeaderLogOut();
-echo MainPage::mainPageNav();
-echo MainPage::mainPageAdv();
-echo MainPage::mainPageRestaurant();
-echo MainPage::mainPageFooter();
+// echo MainPage::mainPageHeaderLogOut();
+// echo MainPage::mainPageNav();
+// echo MainPage::mainPageAdv();
+// echo MainPage::mainPageRestaurant();
+// echo MainPage::mainPageFooter();
 echo Page::htmlEnd();
