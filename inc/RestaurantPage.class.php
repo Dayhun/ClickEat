23c1d30e4289523a115ef0e4352f38f23c793d30
Jacques Ehrardt <ehrardt38@gmail.com>
2023-06-07 15:50:37 -0700

    Menu DAO and Class

--- inc/RestaurantPage.class.php
+++ inc/RestaurantPage.class.php
@@ -27,111 +27,19 @@ class RestaurantPage {
         ';
         return $restaurantTopSection;
     }
-    public static function restaurantMenuSection() {
+    public static function restaurantMenuSection(array $restaurantMenu) {
         $restaurantMenuSection = '
         <section class="restaurantMenu">
             <article class="orderArticle orderNone">
                 <button class="orderBtn">Order Now</button>
             </article>
-            <article class="menuArticle">
-                <figure>
-                    <figcaption>
-                        <article class="menuTitle">
-                            <h4>Big Mac</h4>
-                            <p>$7.69</p>
-                        </article>
-                        <section class="menuBtn">
-                            <button class="minusBtn">-</button>
-                            <span>0</span>
-                            <button class="plusBtn">+</button>
-                        </section>
-                    </figcaption>
-                    <img src="./img/burger.png" alt="burger-img"/>
-                </figure>
-                <figure>
-                    <figcaption>
-                        <article class="menuTitle">
-                            <h4>Big Mac</h4>
-                            <p>$7.69</p>
-                        </article>
-                        <section class="menuBtn">
-                            <button class="minusBtn">-</button>
-                            <span>0</span>
-                            <button class="plusBtn">+</button>
-                        </section>
-                    </figcaption>
-                    <img src="./img/burger.png" alt="burger-img"/>
-                </figure>
-                <figure>
-                    <figcaption>
-                        <article class="menuTitle">
-                            <h4>Big Mac</h4>
-                            <p>$7.69</p>
-                        </article>
-                        <section class="menuBtn">
-                            <button class="minusBtn">-</button>
-                            <span>0</span>
-                            <button class="plusBtn">+</button>
-                        </section>
-                    </figcaption>
-                    <img src="./img/burger.png" alt="burger-img"/>
-                </figure>
-                <figure>
-                    <figcaption>
-                        <article class="menuTitle">
-                            <h4>Big Mac</h4>
-                            <p>$7.69</p>
-                        </article>
-                        <section class="menuBtn">
-                            <button class="minusBtn">-</button>
-                            <span>0</span>
-                            <button class="plusBtn">+</button>
-                        </section>
-                    </figcaption>
-                    <img src="./img/burger.png" alt="burger-img"/>
-                </figure>
-                <figure>
-                    <figcaption>
-                        <article class="menuTitle">
-                            <h4>Big Mac</h4>
-                            <p>$7.69</p>
-                        </article>
-                        <section class="menuBtn">
-                            <button class="minusBtn">-</button>
-                            <span>0</span>
-                            <button class="plusBtn">+</button>
-                        </section>
-                    </figcaption>
-                    <img src="./img/burger.png" alt="burger-img"/>
-                </figure>
-                <figure>
-                    <figcaption>
-                        <article class="menuTitle">
-                            <h4>Big Mac</h4>
-                            <p>$7.69</p>
-                        </article>
-                        <section class="menuBtn">
-                            <button class="minusBtn">-</button>
-                            <span>0</span>
-                            <button class="plusBtn">+</button>
-                        </section>
-                    </figcaption>
-                    <img src="./img/burger.png" alt="burger-img"/>
-                </figure>
-                <figure>
-                    <figcaption>
-                        <article class="menuTitle">
-                            <h4>Big Mac</h4>
-                            <p>$7.69</p>
-                        </article>
-                        <section class="menuBtn">
-                            <button class="minusBtn">-</button>
-                            <span>0</span>
-                            <button class="plusBtn">+</button>
-                        </section>
-                    </figcaption>
-                    <img src="./img/burger.png" alt="burger-img"/>
-                </figure>
+            <article class="menuArticle">';
+        
+        foreach($restaurantMenu as $menuInfo){
+            $restaurantMenuSection .= self::menuInfo($menuInfo);
+        }
+            
+        $restaurantMenuSection .= '
             </article>    
             <article class="orderArticle">
                 <button class="orderBtn">Order Now</button>
@@ -139,5 +47,26 @@ class RestaurantPage {
         </section>
         ';
         return $restaurantMenuSection;
+    }
+
+    private static function menuInfo($menuInfo){
+        $menuString = '
+            <figure>
+                <figcaption>
+                    <article class="menuTitle">
+                        <h4>'. $menuInfo->getName() .'</h4>
+                        <p>$'. $menuInfo->getPrice() .'</p>
+                    </article>
+                    <section class="menuBtn">
+                        <button class="minusBtn">-</button>
+                        <span>0</span>
+                        <button class="plusBtn">+</button>
+                    </section>
+                </figcaption>
+                <img src="'. $menuInfo->getPicture() .'" alt="burger-img"/>
+            </figure>
+        ';
+        
+        return $menuString;
     }
 }
--- restaurantPage.php
+++ restaurantPage.php
@@ -7,13 +7,16 @@ require_once("inc/Page.class.php");
 require_once("./inc/Utilities/PDOAgent.class.php");
 
 require_once("./inc/Entities/Restaurant.class.php");
+require_once("./inc/Entities/Menu.class.php");
 require_once("./inc/Utilities/DAO/RestaurantDAO.class.php");
+require_once("./inc/Utilities/DAO/MenuDAO.class.php");
 
 require_once("inc/LoginManager.class.php");
 require_once("inc/MainPage.class.php");
 require_once("inc/RestaurantPage.class.php");
 
 RestaurantDAO::init();
+MenuDAO::init();
 
 echo Page::htmlStart();
 echo MainPage::mainPageHeader();
@@ -21,9 +24,10 @@ echo MainPage::mainPageHeader();
 if(!empty($_GET)){
     if(!empty($_GET['restaurantId'])){
         $restaurantId = intval($_GET['restaurantId']);
-        $restaurant = RestaurantDAO::getRestaurantsById($restaurantId);
-        echo RestaurantPage::restaurantTopSection($restaurant);
-        echo RestaurantPage::restaurantMenuSection();
+        $restaurantInfo = RestaurantDAO::getRestaurantsById($restaurantId);
+        $restaurantMenu = MenuDAO::getMenuByRestaurantId($restaurantId);
+        echo RestaurantPage::restaurantTopSection($restaurantInfo);
+        echo RestaurantPage::restaurantMenuSection($restaurantMenu);
     }
 }
 
