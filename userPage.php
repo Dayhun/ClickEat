68e5f1534af2fa37d517540ab02b5bdacd2c1d7f
Gabriel <bielpch@gmail.com>
2023-06-07 20:55:39 -0700

    php

--- userPage.php
+++ userPage.php
@@ -1,9 +1,18 @@
 <?php
 require_once("./inc/Page.class.php");
+require_once("./inc/LoginManager.class.php");
+
+require_once("./inc/Entities/User.class.php");
+
 require_once("./inc/Userpage.class.php");
 
-
+session_start();
 
 echo Page::htmlStart();
-echo Userpage::Userpage();
-echo Page::htmlEnd();
+
+if(LoginManager::verifyLogin()){
+    $user = $_SESSION["email"];
+    echo Userpage::Userpage($user);
+ }
+
+ echo Page::htmlEnd();
